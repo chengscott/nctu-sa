1e7a1d2bb83770deef530ae471d979e330a8b907
chengscott <60510scott@gmail.com>
2018-10-22 15:37:33 +0800

    reorder function

--- hw2/hw2-2.sh
+++ hw2/hw2-2.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
-rprint() { for _ in $(seq "$1"); do echo -n "$2"; done }
-
 
 # display table helper function
+calculate_CH() {
+    # calculate column height from ${course_table} into ${CH}
+    [ "$show_classroom" = 'on' ] && cf='-f3,4' || cf='-f3'
+    for cos in $course_table; do
+        len=$(echo "$cos" | cut -d'@' "$cf" | wc -c)
+        col=$(( (len + CW - 1) / CW ))
+        CH=$(( CH > col ? CH : col ))
+    done
+}
+
+parse_cos_table() {
+    # parse ${course_table} into ${cos_table}
+    for cos in $course_table; do
+        name=$(echo "$cos" | cut -d'@' -f3,4)
+        time=$(echo "$cos" | cut -d'@' -f2 | sed -e 's/\(.\)/\1 /g')
+        d='0'
+        for t in $time; do
+            case $t in
+                (*[!0-9]*|'') cos_table="$cos_table $d$t@$name";;
+                (*)           d=$t;;
+            esac
+        done
+    done
+}
+
+search_cos_table() {
+    # search "$1" in ${cos_table}, and the search result is ${name}
+    [ "$show_classroom" = 'on' ] && cf='-f2,3' || cf='-f2'
+    for cos in $cos_table; do
+        name=$(echo "$cos" | grep "^$1" | cut -d'@' "$cf")
+        [ -z "$name" ] || return 0
+    done
+}
+
+rprint() { for _ in $(seq "$1"); do echo -n "$2"; done }
+
 draw_table() {
     # draw table using search_cos_table() and
     # ${DAY}, ${DAY_N}, ${TIME}, $W, $H, ${CW}, ${CH}
@@ -43,48 +77,16 @@ draw_table() {
     done
 }
 
-calculate_CH() {
-    # calculate column height from ${course_table} into ${CH}
-    [ "$show_classroom" = 'on' ] && cf='-f3,4' || cf='-f3'
-    for cos in $course_table; do
-        len=$(echo "$cos" | cut -d'@' "$cf" | wc -c)
-        col=$(( (len + CW - 1) / CW ))
-        CH=$(( CH > col ? CH : col ))
-    done
-}
-
-parse_cos_table() {
-    # parse ${course_table} into ${cos_table}
-    for cos in $course_table; do
-        name=$(echo "$cos" | cut -d'@' -f3,4)
-        time=$(echo "$cos" | cut -d'@' -f2 | sed -e 's/\(.\)/\1 /g')
-        d='0'
-        for t in $time; do
-            case $t in
-                (*[!0-9]*|'') cos_table="$cos_table $d$t@$name";;
-                (*)           d=$t;;
-            esac
-        done
-    done
-}
-
-search_cos_table() {
-    # search "$1" in ${cos_table}, and result is ${name}
-    [ "$show_classroom" = 'on' ] && cf='-f2,3' || cf='-f2'
-    for cos in $cos_table; do
-        name=$(echo "$cos" | grep "^$1" | cut -d'@' "$cf")
-        [ -z "$name" ] || return 0
-    done
-}
-
 parse_display_table() {
     calculate_CH
     parse_cos_table
     display_table=$(draw_table)
 }
 
+
 # add course helper function
 check_collision() {
+    # check "$1" in ${cos_table}, and the collision result is ${collision}
     time=$(echo "$1" | cut -d'@' -f2 | sed -e 's/\(.\)/\1 /g')
     collision=''
     d='0'
@@ -100,6 +102,115 @@ check_collision() {
         name=$(echo "$1" | cut -d'@' -f3 | sed -e 's/~/ /g')
         collision="Collision: $d$t\n$name"
     fi
+}
+
+parse_course_menu() {
+    # parse ${all_course} into ${course_menu}
+    course_menu=''
+    all_course_count=0
+    check=0
+    [ "$show_collision" = 'Show_All' ] && check=1
+    search_name=$(echo "$search_name" | sed -e 's/ /~/g')
+    search_time=$(echo "$search_time" | sed -e 's/\(.\)/\1 /g')
+    for cos in $all_course; do
+        if [ "$check" = "1" ]; then
+            check_collision "$cos"
+            [ -n "$collision" ] && continue
+        fi
+        if [ -n "$search_name" ]; then
+            match=$(echo "$cos" | cut -d'@' -f3 | grep -c "$search_name")
+            [ "$match" = '0' ] && continue
+        elif [ -n "$search_time" ]; then
+            time=$(echo "$cos" | cut -d'@' -f2 | sed -e 's/\(.\)/\1 /g')
+            ctime=''
+            d=0
+            for t in $time; do
+                case $t in
+                    (*[!0-9]*|'') ctime="$d$t@$ctime";;
+                    (*) d=$t;;
+                esac
+            done
+            match=1
+            d=0
+            for t in $search_time; do
+                case $t in
+                    (*[!0-9]*|'')
+                        [ "$match" = '1' ] && match=$(echo "$ctime" | grep -c "$d$t")
+                        ;;
+                    (*) d=$t;;
+                esac
+            done
+            [ "$match" = '0' ] && continue
+        fi
+        cid=$(echo "$cos" | cut -d'@' -f1)
+        entry=$(echo "$cos" | awk -F '@' '{
+            printf "%s %s - ", $2, $4
+            $0 = $3
+            gsub(/~/, " ")
+            printf $0
+        }')
+        course_menu="$course_menu $cid \"$entry\""
+        all_course_count=$(( all_course_count + 1 ))
+    done
+}
+
+save_config() {
+    # save ${table_option} and ${course_table} into hw2.cfg
+    echo "$table_option" > hw2.cfg
+    echo '' >> hw2.cfg
+    for cos in $course_table; do
+        echo "$cos" >> hw2.cfg
+    done
+}
+
+
+# option helper function
+parse_table_option() {
+    # parse ${table_option} into global info
+    DAY='Mon Tue Wed Thu Fri '
+    DAY_N='1 2 3 4 5'
+    TIME='A B C D E F G H I J K'
+    show_classroom='off'
+    show_nocoll='off'
+    show_collision='Hide_Collision'
+    for opt in $table_option; do
+        case $opt in
+            1)
+                DAY='Mon Tue Wed Thu Fri Sat Sun '
+                DAY_N='1 2 3 4 5 6 7'
+                ;;
+            2)
+                TIME='M N A B C D X E F G H Y I J K L'
+                ;;
+            3)
+                show_classroom='on'
+                ;;
+            4)
+                show_nocoll='on'
+                show_collision='Show_All'
+                ;;
+        esac
+    done
+    CW=$(( W / (${#DAY} / 4) - 3 ))
+}
+
+save_table_option() {
+    # save ${table_option} into hw2.cfg
+    sed -i '' -e "1 s/^.*$/$table_option/" hw2.cfg
+}
+
+
+# dialog helper function
+dialog_main() {
+    tmpfile=$(mktemp /tmp/hw2.XXX)
+    echo "$display_table" > "$tmpfile"
+    dialog --ok-label 'Add Course' --extra-button --extra-label 'Option' \
+        --help-button --help-label 'Exit' \
+        --textbox "$tmpfile" "$H" "$W"
+    option=$?
+    rm -f "$tmpfile"
+    #trap "rm -f $tmpfile" 1 2 3 15
+    return "$option"
 }
 
 course_state() {
@@ -124,20 +235,6 @@ course_state() {
                 ;;
         esac
     done
-}
-
-
-# dialog helper function
-dialog_main() {
-    tmpfile=$(mktemp /tmp/hw2.XXX)
-    echo "$display_table" > "$tmpfile"
-    dialog --ok-label 'Add Course' --extra-button --extra-label 'Option' \
-        --help-button --help-label 'Exit' \
-        --textbox "$tmpfile" "$H" "$W"
-    option=$?
-    rm -f "$tmpfile"
-    #trap "rm -f $tmpfile" 1 2 3 15
-    return "$option"
 }
 
 dialog_course() {
@@ -239,97 +336,6 @@ load_table_option() {
     fi
 }
 
-
-# config saving helper function
-save_config() {
-    echo "$table_option" > hw2.cfg
-    echo '' >> hw2.cfg
-    for cos in $course_table; do
-        echo "$cos" >> hw2.cfg
-    done
-}
-
-save_table_option() {
-    sed -i -e "1 s/^.*$/$table_option/" hw2.cfg
-}
-
-# config paring helper function
-parse_course_menu() {
-    course_menu=''
-    all_course_count=0
-    check=0
-    [ "$show_collision" = 'Show_All' ] && check=1
-    search_name=$(echo "$search_name" | sed -e 's/ /~/g')
-    search_time=$(echo "$search_time" | sed -e 's/\(.\)/\1 /g')
-    for cos in $all_course; do
-        if [ "$check" = "1" ]; then
-            check_collision "$cos"
-            [ -n "$collision" ] && continue
-        fi
-        if [ -n "$search_name" ]; then
-            match=$(echo "$cos" | cut -d'@' -f3 | grep -c "$search_name")
-            [ "$match" = '0' ] && continue
-        elif [ -n "$search_time" ]; then
-            time=$(echo "$cos" | cut -d'@' -f2 | sed -e 's/\(.\)/\1 /g')
-            ctime=''
-            d=0
-            for t in $time; do
-                case $t in
-                    (*[!0-9]*|'') ctime="$d$t@$ctime";;
-                    (*) d=$t;;
-                esac
-            done
-            match=1
-            d=0
-            for t in $search_time; do
-                case $t in
-                    (*[!0-9]*|'')
-                        [ "$match" = '1' ] && match=$(echo "$ctime" | grep -c "$d$t")
-                        ;;
-                    (*) d=$t;;
-                esac
-            done
-            [ "$match" = '0' ] && continue
-        fi
-        cid=$(echo "$cos" | cut -d'@' -f1)
-        entry=$(echo "$cos" | awk -F '@' '{
-            printf "%s %s - ", $2, $4
-            $0 = $3
-            gsub(/~/, " ")
-            printf $0
-        }')
-        course_menu="$course_menu $cid \"$entry\""
-        all_course_count=$(( all_course_count + 1 ))
-    done
-}
-
-parse_table_option() {
-    DAY='Mon Tue Wed Thu Fri '
-    DAY_N='1 2 3 4 5'
-    TIME='A B C D E F G H I J K'
-    show_classroom='off'
-    show_nocoll='off'
-    show_collision='Hide_Collision'
-    for opt in $table_option; do
-        case $opt in
-            1)
-                DAY='Mon Tue Wed Thu Fri Sat Sun '
-                DAY_N='1 2 3 4 5 6 7'
-                ;;
-            2)
-                TIME='M N A B C D X E F G H Y I J K L'
-                ;;
-            3)
-                show_classroom='on'
-                ;;
-            4)
-                show_nocoll='on'
-                show_collision='Show_All'
-                ;;
-        esac
-    done
-    CW=$(( W / (${#DAY} / 4) - 3 ))
-}
 
 # global info
 W=$(tput cols)
